967827012c103df421b5415608f656a187f256ef
cong <dangcong21205@gmail.com>
2025-10-19 17:35:21 +0700

    Trang chủ

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeatureDestination from '../components/FeatureDestination'
+import ExclusiveOffers from '../components/ExclusiveOffers'
+
+const Home = () => {
+  return (
+    <>
+      <Hero/>
+      <FeatureDestination/>
+      <ExclusiveOffers/>
+    </>
+  )
+}
+
+export default Home
