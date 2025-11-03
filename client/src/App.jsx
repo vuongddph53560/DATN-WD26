893b7721d9c9c6831b45a05760a28cf1eef3aa8f
cong <dangcong21205@gmail.com>
2025-11-03 11:04:59 +0700

    Toàn Bộ Client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,7 @@ import Home from './pages/Home';
 import Footer from './components/Footer';
 import AllRooms from './pages/AllRooms';
 import RoomDetail from './pages/RoomDetail';
-import Mybooking from './pages/Mybookings';
-import HotelReg from './components/HotelReg';
-import Layout from './pages/hotelOwner/Layout';
-import Dashboard from './pages/hotelOwner/Dashboard';
-import AddRoom from './pages/hotelOwner/AddRoom';
-import ListRoom from './pages/hotelOwner/ListRoom';
+import MyBookings from './pages/MyBookings';
 
 const App = () => {
 
@@ -19,18 +14,12 @@ const App = () => {
   return (
     <div>
      { !isOwnerPath && <Navbar />}
-     { false  &&  <HotelReg />}
      <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/rooms' element={<AllRooms/>}/>
           <Route path='/rooms/:id' element={<RoomDetail/>}/>
-          <Route path='/my-bookings' element={<Mybooking/>}/>
-          <Route path='/owner' element={<Layout/>}>
-              <Route index element={<Dashboard/>}/>
-              <Route path="add-room" element={<AddRoom/>}/>
-              <Route path="list-room" element={<ListRoom/>}/>
-          </Route>
+          <Route path='/my-bookings' element={<MyBookings/>}/>
         </Routes>
      </div>
      <Footer/>
