e3b7a13e99434a3fed2e70736329348678fa3e83
Duanmer <duanpham0109@gmail.com>
2025-11-06 00:21:00 +0700

    update-creat admin

--- client/src/pages/hotelOwner/Dashboard.jsx
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Title from '../../components/Title'
+import { assets, dashboardDummyData } from '../../assets/assets'
 
 const Dashboard = () => {
+
+  const [dashboardData, setDashboardData] = useState(dashboardDummyData)
   return (
-    <div>Dashboard</div>
+    <div>
+      < Title align='left' font='outfit' title='Dashboard' subTitle='' />
+      <div className='flex grap-4 my-8'>
+        {/*  */}
+        <div className='bg-primary/3 border border-primary/10 rounded flex p-4 pr-8'>
+          <img src={assets.totalBookingIcon} alt="" className='max-sm:hidden h-10' />
+          <div className='flex flex-col sm:ml-4 font-medium'>
+            <p className='text-blue-500 text-lg'>Tổng số đặt phòng</p>
+            <p className='text-neutral-400 text-base'>{dashboardData.totalBookings}</p>
+          </div>
+
+        </div>
+        {/* */}
+        <div className='bg-primary/3 border border-primary/10 rounded flex p-4 pr-8'>
+          <img src={assets.totalRevenueIcon} alt="" className='max-sm:hidden h-10' />
+          <div className='flex flex-col sm:ml-4 font-medium'>
+            <p className='text-blue-500 text-lg'>Tổng doanh thu</p>
+            <p className='text-neutral-400 text-base'>{dashboardData.totalRevenue} VND</p>
+          </div>
+
+        </div>
+      </div>
+      {/* */}
+      <h2 className='text-xl text-blue-950/70 font-medium mb-5'>Đặt phòng gần đây</h2>
+      <div className='w-full max-w-3xl text-left border border-gray-300 rounded-1g max-h-80 overflow-y-scroll'>
+        <table className='w-full'>
+          <thead className='bg-gray-50'>
+            <tr>
+              <th className='py-3 px-4 text-gray-800 font-medium'>Tên người dùng</th>
+              <th className='py-3 px-4 text-gray-800 font-medium max-sm:hidden'>Tên phòng</th>
+              <th className='py-3 px-4 text-gray-800 font-medium text-center'>Tổng số tiền</th>
+              <th className='py-3 px-4 text-gray-800 font-medium text-center'>Trạng thái</th>
+            </tr>
+          </thead>
+          <tbody className='text-sm'>
+            {dashboardData.bookings.map((item, index) => (
+              <tr key={index}>
+                <td className='py-3 px-4 text-gray-700 border-t border-gray-300'>
+                  {item.user.username}
+                </td>
+
+                <td className='py-3 px-4 text-gray-700 border-t border-gray-300 max-sm:hidden'>
+                  {item.room.roomType}
+                </td>
+
+                <td className='py-3 px-4 text-gray-700 border-t border-gray-300 text-center'>
+                  {item.totalPrice} VND
+                </td>
+                <td className='py-3 px-4 border-t border-gray-300 flex'>
+                  <button className={`py-1 px-3 text-xs rounded-full max-full mx-auto ${item.isPaid ? 'bg-green-200 text-green-600' : 'bg-amber-200 text-yellow-600'}`}>
+                    {item.isPaid ? 'Hoàn thành' : 'Chờ xử lý'}
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
