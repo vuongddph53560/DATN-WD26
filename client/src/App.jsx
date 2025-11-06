8549d144c287431008d472790dfe35684824fa0d
Duanmer <duanpham0109@gmail.com>
2025-11-06 20:56:20 +0700

    Cập nhật code mới

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,25 +4,30 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import AllRooms from './pages/AllRooms';
-import RoomDetail from './pages/RoomDetail';
-import MyBookings from './pages/MyBookings';
+import Mybooking from './pages/Mybookings';
 import HotelReg from './components/HotelReg';
 import Layout from './pages/hotelOwner/Layout';
+import Dashboard from './pages/hotelOwner/Dashboard';
+import AddRoom from './pages/hotelOwner/AddRoom';
+import ListRoom from './pages/hotelOwner/ListRoom';
 
 const App = () => {
 
   const isOwnerPath = useLocation().pathname.includes("owner");
+
   return (
     <div>
      { !isOwnerPath && <Navbar />}
-     {false && <HotelReg/>}
+     { false  &&  <HotelReg />}
      <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/rooms' element={<AllRooms/>}/>
-          <Route path='/rooms/:id' element={<RoomDetail/>}/>
-          <Route path='/my-bookings' element={<MyBookings/>}/>
+          <Route path='/my-bookings' element={<Mybooking/>}/>
           <Route path='/owner' element={<Layout/>}>
+              <Route index element={<Dashboard/>}/>
+              <Route path="add-room" element={<AddRoom/>}/>
+              <Route path="list-room" element={<ListRoom/>}/>
 
           </Route>
         </Routes>
