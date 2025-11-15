347efa5ceca37ddb652258f84cc045b8c1a3b7d9
vuong <vuongddph53560@gmail.com>
2025-11-15 21:24:46 +0700

    fix loi server & fix components cilent

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import { Route, Routes, useLocation } from 'react-router-dom';
-import Home from './pages/Home';
-import Footer from './components/Footer';
-import AllRooms from './pages/AllRooms';
-import Mybooking from './pages/Mybookings';
-import HotelReg from './components/HotelReg';
-import Layout from './pages/hotelOwner/Layout';
-import Dashboard from './pages/hotelOwner/Dashboard';
-import AddRoom from './pages/hotelOwner/AddRoom';
-import ListRoom from './pages/hotelOwner/ListRoom';
+import Home from './pages/Home.jsx';
+import Footer from './components/Footer.jsx';
+import AllRooms from './pages/AllRooms.jsx';
+import Mybooking from './pages/Mybookings.jsx';
+import HotelReg from './components/HotelReg.jsx';
+import Layout from './pages/hotelOwner/Layout.jsx';
+import Dashboard from './pages/hotelOwner/Dashboard.jsx';
+import AddRoom from './pages/hotelOwner/AddRoom.jsx';
+import ListRoom from './pages/hotelOwner/ListRoom.jsx';
 import RoomDetail from './pages/RoomDetail';
 import {Toaster} from 'react-hot-toast'
-import { useAppContext } from './conext/AppContext';
+import { useAppContext } from './conext/AppContext.jsx';
 
 const App = () => {
 
