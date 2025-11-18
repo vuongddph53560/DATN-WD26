aeb667eb18e8b8e85ce1e753b0fe7782e6facd08
cong <dangcong21205@gmail.com>
2025-11-18 21:32:04 +0700

    Luu code

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         mongoose.connection.on('connected', ()=> console.log("MongoDB connected successfully"));
-        await mongoose.connect(`${process.env.MONGODB_URL}/hotel-booking`);
+        await mongoose.connect(`${process.env.MONGODB_URL}`);
     }catch(error){
         console.log(error.message);
     }
