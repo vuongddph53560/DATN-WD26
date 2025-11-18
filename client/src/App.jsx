11e156931bee3b3a69114a0af45d8ee4fd33bf8c
Duanmer <duanpham0109@gmail.com>
2025-11-18 13:07:05 +0700

    Fix lỗi tạo phòng và cập nhật router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,21 +11,24 @@ import Dashboard from './pages/hotelOwner/Dashboard';
 import AddRoom from './pages/hotelOwner/AddRoom';
 import ListRoom from './pages/hotelOwner/ListRoom';
 import RoomDetail from './pages/RoomDetail';
+import {Toaster} from 'react-hot-toast'
+import { useAppContext } from './conext/AppContext';
 
 const App = () => {
 
   const isOwnerPath = useLocation().pathname.includes("owner");
-
+  const {showHotelReg} = useAppContext();
   return (
     <div>
+      <Toaster/>
      { !isOwnerPath && <Navbar />}
-     { false  &&  <HotelReg />}
+     { showHotelReg  &&  <HotelReg />}
      <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/rooms' element={<AllRooms/>}/>
+          <Route path='/rooms/:id' element={<RoomDetail/>}/>
           <Route path='/my-bookings' element={<Mybooking/>}/>
-          <Route path="/rooms/:id" element={<RoomDetail />} />
           <Route path='/owner' element={<Layout/>}>
               <Route index element={<Dashboard/>}/>
               <Route path="add-room" element={<AddRoom/>}/>
